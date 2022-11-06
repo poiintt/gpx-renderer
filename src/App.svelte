9185277bd82abbf55e7e7c6de05a8f5e9af4c625
poiintt <abendmo@gmail.com>
2022-11-06 18:48:31 +0100

    feat: upload and view gpx routes
    - install d3-geo, @tmcw/togeojson
    - remove unused files

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,62 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { gpx } from "@tmcw/togeojson";
+  import { geoMercator } from "d3-geo";
+
+  let files: FileList;
+  const width = 800;
+  const height = 600;
+
+  async function getPath(file: File) {
+    const xml = await file.text();
+    const document = new DOMParser().parseFromString(xml, "text/xml");
+    const featureCollection = gpx(document);
+
+    if ("coordinates" in featureCollection.features[0].geometry) {
+      const coordinates = featureCollection.features[0].geometry
+        .coordinates as [number, number, number][];
+
+      const padding = 20;
+      const geoProjection = geoMercator().fitExtent(
+        [
+          [padding, padding],
+          [width - padding * 2, height - padding * 2],
+        ],
+        featureCollection
+      );
+
+      const mappedCoordinates = coordinates.map((cord) =>
+        geoProjection([cord[0], cord[1]])
+      );
+      const path = "M " + mappedCoordinates.join(" L ");
+      return path;
+    }
+    return "";
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<label for="route">Upload a GPX file:</label>
+<input accept=".gpx" bind:files id="route" name="route" type="file" />
 
-  <div class="card">
-    <Counter />
-  </div>
+{#if files}
+  <h2>Selected files:</h2>
+  {#each Array.from(files) as file}
+    <p>{file.name} ({file.size} bytes)</p>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+    {#await getPath(file)}
+      <p>...loading map</p>
+    {:then d}
+      <svg {width} {height}>
+        <path
+          {d}
+          fill="none"
+          stroke="red"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-miterlimit="4"
+        />
+      </svg>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  {/each}
+{/if}
